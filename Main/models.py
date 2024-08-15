6bc64a4695c54288a0d8db720b9c393796090b7f
Kali Live user <hasantechninja@gmail.com>
2024-08-15 21:15:52 +0600

    update

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+type_choice = (
+    ('IT','IT'),
+    ('Computer', 'Computer'),
+    ('Mobile','Mobile')
+
+)
+
+class Company(models.Model):
+    company_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    about = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices= type_choice)
+    added_date = models.DateField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    
--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from . models import *
+from . serializers import *
 
 # Create your views here.
 
 def Home(request):
     return render(request, 'home.html')
+
+
+def CompanyViews(request):
+    return render(request, 'company.html')
